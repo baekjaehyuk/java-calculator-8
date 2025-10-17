ba583fddf4b98b59c7e308ae0290d97104687fc7
baekjaehyuk <bjh010321@naver.com>
2025-10-17 23:15:39 +0900

    feat: AppConfig를 이용한 의존성 주입 설정

---
+++ src/main/java/calculator/config/AppConfig.java
@@ -0,0 +1,46 @@
+package calculator.config;
+
+import calculator.controller.CalculatorController;
+import calculator.model.StringCalculator;
+import calculator.parser.CustomDelimiterParser;
+import calculator.parser.DefaultDelimiterParser;
+import calculator.parser.Parser;
+import calculator.parser.handler.ParserHandler;
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class AppConfig {
+
+    public CalculatorController calculatorController() {
+        return new CalculatorController(calculatorService(), inputView(), outputView());
+    }
+
+    public CalculatorService calculatorService() {
+        return new CalculatorService(stringCalculator(), parserHandler());
+    }
+
+    public StringCalculator stringCalculator() {
+        return new StringCalculator();
+    }
+
+    public ParserHandler parserHandler() {
+        return new ParserHandler(defaultDelimiterParser(), customDelimiterParser());
+    }
+ㅈ
+    public Parser defaultDelimiterParser() {
+        return new DefaultDelimiterParser();
+    }
+
+    public Parser customDelimiterParser() {
+        return new CustomDelimiterParser();
+    }
+
+    public InputView inputView() {
+        return new InputView();
+    }
+
+    public OutputView outputView() {
+        return new OutputView();
+    }
+}

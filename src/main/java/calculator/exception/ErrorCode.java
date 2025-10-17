9769b5dce35c200ecfe4221577260e8832d29a71
baekjaehyuk <bjh010321@naver.com>
2025-10-17 23:54:50 +0900

    style: 예외 메시지 수정

--- src/main/java/calculator/exception/ErrorCode.java
+++ src/main/java/calculator/exception/ErrorCode.java
@@ -4,8 +4,9 @@ public enum ErrorCode {
 
     NEGATIVE_NUMBER("음수는 허용되지 않습니다: "),
     INVALID_DELIMITER_FORMAT("구분자 형식이 올바르지 않습니다: "),
-    UNSUPPORTED_PARSER_FORMAT("지원하지 않는 파서 형식입니다: "),
-    INVALID_NUMBER_FORMAT("유효하지 않은 값이 포함되어 있습니다: ");
+    UNSUPPORTED_PARSER_FORMAT("파싱할 수 없는 형식입니다: "),
+    INVALID_NUMBER_FORMAT("유효하지 않은 값이 포함되어 있습니다: "),
+    INPUT_CANNOT_BE_NULL("null은 허용하지 않습니다.");
 
     private final String message;
 

d17c671dcafc322c88a00409b30427a12d9eff8b
baekjaehyuk <bjh010321@naver.com>
2025-10-17 23:20:09 +0900

    feat: 애플리케이션 흐름을 제어하는 CalculatorController 구현

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,29 @@
+package calculator.controller;
+
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CalculatorController(CalculatorService calculatorService, InputView inputView, OutputView outputView) {
+        this.calculatorService = calculatorService;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        try {
+            String input = inputView.getInput();
+            int result = calculatorService.calculate(input);
+            outputView.printResult(result);
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            throw e;
+        }
+    }
+}
